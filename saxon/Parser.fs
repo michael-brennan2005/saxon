9e8769430b6ea58d5c3f30ce92e42c8f49337162
Tech0tron <tech0tron.roblox@gmail.com>
2022-02-02 10:59:51 -0700

    parser pt.2
    parens

--- saxon/Parser.fs
+++ saxon/Parser.fs
@@ -1,4 +1,5 @@
-﻿module saxon.Parser
+﻿// "rec"-ing namespace is a code smell. but who cares
+module rec saxon.Parser
 
 open saxon.Tokenizer
 
@@ -14,9 +15,26 @@ type Node =
     | Number of double
     | Null // todo: fix this when we actually want to implement error handling
 
+// assumes left paren was already consumed
+let rec insideParens (insideTokens: Token list) (remainingTokens: Token list) (lp: int) (rp: int) =
+    match remainingTokens with
+    | Token.LeftParen :: tail ->
+        insideParens ( Token.LeftParen :: insideTokens ) tail (lp + 1) rp
+    | Token.RightParen :: tail when rp + 1 = lp ->
+        (insideTokens |> List.rev, tail)
+    | Token.RightParen :: tail ->
+        insideParens (Token.RightParen :: insideTokens ) tail lp (rp + 1)
+    | [] ->
+        (insideTokens |> List.rev, remainingTokens)
+    | token :: tail ->
+        insideParens (token :: insideTokens) tail lp rp
+      
 let primary (tokens: Token list) =
     match tokens with
     | Token.Number(num) :: tail  -> (Node.Number(num), tail)
+    | Token.LeftParen :: tail ->
+        let (insideTokens, remainingTokens) = insideParens [] tail 1 0
+        (expression insideTokens, remainingTokens)
     | _ -> (Node.Null, tokens)
 
 let rec exponent (tokens: Token list) =
@@ -28,7 +46,7 @@ let rec exponent (tokens: Token list) =
         (Node.Operation(left, Operator.Exp, right), tokens)
     | _ ->
         (left, tokens)
-       
+ 
 let rec product (tokens: Token list) =
     let left, tokens = exponent(tokens)
     
