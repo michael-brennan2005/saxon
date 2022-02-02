399f149d0dca71a2451c97f4377255df052191f2
Tech0tron <tech0tron.roblox@gmail.com>
2022-02-01 22:02:41 -0700

    parser pt. 1
    todo: add parens and unary -

--- saxon/Parser.fs
+++ saxon/Parser.fs
@@ -1,2 +1,60 @@
 ﻿module saxon.Parser
 
+open saxon.Tokenizer
+
+type Operator =
+    | Add
+    | Sub
+    | Mul
+    | Div
+    | Exp
+
+type Node =
+    | Operation of Node * Operator * Node
+    | Number of double
+    | Null // todo: fix this when we actually want to implement error handling
+
+let primary (tokens: Token list) =
+    match tokens with
+    | Token.Number(num) :: tail  -> (Node.Number(num), tail)
+    | _ -> (Node.Null, tokens)
+
+let rec exponent (tokens: Token list) =
+    let left, tokens = primary(tokens)
+    
+    match tokens with
+    | Token.Exp :: tail ->
+        let right, tokens = exponent(tail)
+        (Node.Operation(left, Operator.Exp, right), tokens)
+    | _ ->
+        (left, tokens)
+       
+let rec product (tokens: Token list) =
+    let left, tokens = exponent(tokens)
+    
+    match tokens with
+    | Token.Mul :: tail ->
+        let right, tokens = product(tail)
+        (Node.Operation(left, Operator.Mul, right), tokens)
+    | Token.Div :: tail ->
+        let right, tokens = product(tail)
+        (Node.Operation(left, Operator.Div, right), tokens)
+    | _ ->
+        (left, tokens)
+        
+let rec sum (tokens: Token list) =
+    let left, tokens = product(tokens)
+    
+    match tokens with
+    | Token.Add :: tail ->
+        let right, tokens = sum(tail)
+        (Node.Operation(left, Operator.Add, right), tokens)
+    | Token.Sub :: tail ->
+        let right, tokens = sum(tail)
+        (Node.Operation(left, Operator.Sub, right), tokens)
+    | _ ->
+        (left, tokens)
+        
+let rec expression (tokens: Token list) =
+    let result, _ = sum tokens
+    result
--- saxon/Program.fs
+++ saxon/Program.fs
@@ -1,4 +1,5 @@
 open System
+open saxon.Parser
 open saxon.Tokenizer
 
 let x = Seq.toList "3.1 + 2.4 ^ 36.23 * 10^-5"
@@ -6,5 +7,7 @@ let y = tokenize x []
 
 while true do
     let input = Console.ReadLine()
-    printTokens (tokenize (Seq.toList input) [])
+    let s1 = tokenize (Seq.toList input) []
+    let s2 = expression s1
+    printfn $"{s2}"
 
