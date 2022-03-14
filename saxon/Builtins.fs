eb1449e6868fcd8647fcc0f4095867cf00537999
Tech0tron <tech0tron.roblox@gmail.com>
2022-03-13 21:21:03 -0600

    builtin functions
    Secant trig, number theory, roots, calc functions, and sum/product

--- saxon/Builtins.fs
+++ saxon/Builtins.fs
@@ -1,6 +1,5 @@
 module saxon.Builtins
 
-open System
 open Microsoft.FSharp.Collections
 open saxon.Interpreter
 open saxon.Parser
@@ -23,6 +22,18 @@ let builtinTan (context: Context) =
     let result, _ = walk (findVariable context "x") context     
     (tan(result), context)
     
+let builtinCsc (context: Context) =
+    let result, _ = walk (findVariable context "x") context     
+    (1.0 / sin(result), context)
+    
+let builtinSec (context: Context) =
+    let result, _ = walk (findVariable context "x") context     
+    (1.0 / cos(result), context)
+    
+let builtinCot (context: Context) =
+    let result, _ = walk (findVariable context "x") context     
+    (1.0 / tan(result), context)
+
 let builtinArccos (context: Context) =
     let result, _ = walk (findVariable context "x") context
     (acos(result), context)
@@ -44,9 +55,50 @@ let builtinLog (context: Context) =
     let y, _ = walk (findVariable context "y") context    
     (log(y) / log(x), context)
     
+let rec gcd (a: float) (b: float) =
+    if a < b then
+        gcd b a
+    else if a % b = 0 then
+        b
+    else 
+        gcd b (a % b)
+
+let builtinGcd (context: Context) =
+    let x, _ = walk (findVariable context "x") context
+    let y, _ = walk (findVariable context "y") context  
+    (gcd x y, context)
+    
+let builtinLcm (context: Context) =
+    let x, _ = walk (findVariable context "x") context
+    let y, _ = walk (findVariable context "y") context  
+    ((x * y) / gcd x y, context)
+    
+let builtinMod (context: Context) =
+    let x, _ = walk (findVariable context "x") context
+    let y, _ = walk (findVariable context "y") context  
+    (x % y, context)
+
+let builtinAbs (context: Context) =
+    let x, _ = walk (findVariable context "x") context
+    (abs(x), context)
+    
+let builtinSqrt (context: Context) =
+    let x, _ = walk (findVariable context "x") context
+    (sqrt(x), context)
+
+let builtinCbrt (context: Context) =
+    let x, _ = walk (findVariable context "x") context
+    (x ** (1.0 / 3.0), context)
+
+let builtinRoot (context: Context) =
+    let x, _ = walk (findVariable context "x") context
+    let y, _ = walk (findVariable context "y") context
+    (x ** (1.0 / y), context)
+
 // Functions that run over purely numerical arguments.
 let builtinNumerical =
     Map.empty
+        // TRIG
         .Add("sin", Function.BuiltinNumerical({
                 FunctionAssignmentInfo.name = "sin";
                 FunctionAssignmentInfo.arguments = ["x"];
@@ -59,6 +111,20 @@ let builtinNumerical =
                 FunctionAssignmentInfo.name = "tan";
                 FunctionAssignmentInfo.arguments = ["x"];
             }, builtinTan))
+        .Add("csc", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "csc";
+                FunctionAssignmentInfo.arguments = ["x"];
+            }, builtinCsc))
+        .Add("sec", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "sec";
+                FunctionAssignmentInfo.arguments = ["x"];
+            }, builtinSec))
+        .Add("cot", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "cot";
+                FunctionAssignmentInfo.arguments = ["x"];
+            }, builtinCot))
+        
+        // INVERSE TRIG
         .Add("arccos", Function.BuiltinNumerical({
                 FunctionAssignmentInfo.name = "arccos";
                 FunctionAssignmentInfo.arguments = ["x"];
@@ -71,6 +137,8 @@ let builtinNumerical =
                 FunctionAssignmentInfo.name = "arctan";
                 FunctionAssignmentInfo.arguments = ["x"];
             }, builtinArctan))
+        
+        // LOGARITHMIC
         .Add("ln", Function.BuiltinNumerical({
                 FunctionAssignmentInfo.name = "ln";
                 FunctionAssignmentInfo.arguments = ["x"];
@@ -79,8 +147,40 @@ let builtinNumerical =
                 FunctionAssignmentInfo.name = "log";
                 FunctionAssignmentInfo.arguments = ["x"; "y";];
             }, builtinLog))
+       
+       // NUMBER
+       .Add("lcm", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "lcm";
+                FunctionAssignmentInfo.arguments = ["x"; "y";];
+            }, builtinLcm))
+        .Add("gcd", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "gcd";
+                FunctionAssignmentInfo.arguments = ["x"; "y";];
+            }, builtinGcd))
+        .Add("mod", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "mod";
+                FunctionAssignmentInfo.arguments = ["x"; "y";];
+            }, builtinMod))
+        .Add("abs", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "abs"
+                FunctionAssignmentInfo.arguments = ["x"];
+            }, builtinAbs))
+        
+        // ROOTS
+        .Add("sqrt", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "sqrt";
+                FunctionAssignmentInfo.arguments = ["x"];
+            }, builtinSqrt))
+        .Add("cbrt", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "cbrt";
+                FunctionAssignmentInfo.arguments = ["x"];
+            }, builtinCbrt))
+        .Add("root", Function.BuiltinNumerical({
+                FunctionAssignmentInfo.name = "mod";
+                FunctionAssignmentInfo.arguments = ["x"; "y";];
+            }, builtinRoot))
 
-let builtinDerive (functionContext: Function) (context: Context) =
+let builtinDerive (functionArg: Function) (context: Context) =
     // difference quotient calculation
     let h = 0.000000001
     let aPlusH =
@@ -89,16 +189,73 @@ let builtinDerive (functionContext: Function) (context: Context) =
         | _ -> h
     
     
-    let ahR, _ = evalFunction functionContext [ Node.Number(aPlusH) ] context
-    let aR, _ = evalFunction functionContext [ findVariable context "a" ] context
+    let ahR, _ = evalFunction functionArg [ Node.Number(aPlusH) ] context
+    let aR, _ = evalFunction functionArg [ findVariable context "a" ] context
     ((ahR - aR) / h, context)
 
+let rec simpsons (functionArg: Function) (context: Context) (amount: float) (a: float) (b: float) (n: int) (step: int) =
+    let h = (b - a) / float n
+    
+    let yn, _ = evalFunction functionArg [ Node.Number(a + float step * h) ] context
+    
+    if step = 0 then
+        simpsons functionArg context yn a b n (step + 1)
+    else if step = n then
+        (h / 3.0) * (amount + yn)
+    else if step % 2 = 1 then
+        simpsons functionArg context (amount + 4.0 * yn) a b n (step + 1)
+    else
+        simpsons functionArg context (amount + 2.0 * yn) a b n (step + 1)
+        
+let builtinIntegrate (functionArg: Function) (context: Context) =
+    let a, _ = walk (findVariable context "a") context
+    let b, _ = walk (findVariable context "b") context
+    ((simpsons functionArg context 0.0 a b 1000 0), context)
+    
+let rec sum (functionArg: Function) (context: Context) (amount: float) (a: float) (b: float) =
+    let y, _ = evalFunction functionArg [ Node.Number(a) ] context
+    
+    if a = b then
+        amount + y
+    else
+        sum functionArg context (amount + y) (a + 1.0) b
+
+let builtinSum (functionArg: Function) (context: Context) = 
+    let a, _ = walk (findVariable context "a") context
+    let b, _ = walk (findVariable context "b") context
+    (sum functionArg context 0.0 a b, context)
+        
+let rec product (functionArg: Function) (context: Context) (amount: float) (a: float) (b: float) =
+    let y, _ = evalFunction functionArg [ Node.Number(a) ] context
+    
+    if a = b then
+        amount * y
+    else
+        product functionArg context (amount * y) (a + 1.0) b
+    
+let builtinProduct (functionArg: Function) (context: Context) =
+    let a, _ = walk (findVariable context "a") context
+    let b, _ = walk (findVariable context "b") context
+    (product functionArg context 1.0 a b, context)
+    
 // Functions that run over possible functional arguments
 // Needed because on builtinnumerical and userdefined the compiler does immediate evaluation
 let builtinFunctional =
     Map.empty
         .Add("derive", Function.BuiltInFunctional({
-            FunctionAssignmentInfo.name = "test";
+            FunctionAssignmentInfo.name = "derive";
             FunctionAssignmentInfo.arguments = ["fx"; "a";];
         }, builtinDerive))
+        .Add("integrate", Function.BuiltInFunctional({
+            FunctionAssignmentInfo.name = "integrate"
+            FunctionAssignmentInfo.arguments = ["fx"; "a"; "b";];
+        }, builtinIntegrate))
+        .Add("sum", Function.BuiltInFunctional({
+            FunctionAssignmentInfo.name = "sum"
+            FunctionAssignmentInfo.arguments = ["fx"; "a"; "b";];
+        }, builtinSum))
+        .Add("product", Function.BuiltInFunctional({
+            FunctionAssignmentInfo.name = "product"
+            FunctionAssignmentInfo.arguments = ["fx"; "a"; "b";];
+        }, builtinProduct))
         
